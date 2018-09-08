2c9e0e492c8f6c9c2da3ab631dd85cee65250e6a
navib <ivan.baena.dev@gmail.com>
2018-09-08 13:53:44 -0400

    updated error in keys.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 module.exports = {
-  googleClientID: '',
-  googleClientSecret: '',
+  googleClientID:
+    '837789257896-v62i0dc9r5pb3sfbjtua5n9f1064n37m.apps.googleusercontent.com',
+  googleClientSecret: 'G7Rs8uTeePdY_-I9SUWtLu19',
   mongoURI:
     'mongodb://fs-admin:fs-adminProd1@ds251002.mlab.com:51002/free-space-prod',
   cookieKey: 'hfiu32r2f908ewi903y4098yrwgfh9w3tg23ougf2eb9u21rb71f'
